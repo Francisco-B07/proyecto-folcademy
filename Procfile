634e899db5557c12ffe12ce7b0f583f35a74eb58
Francisco Burgoa <franciscojburgoa@gmail.com>
2022-07-07 15:39:44 -0300

    modificación de los archivos package.json, Procfile y server.js

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: npm run start:heroku
+node src/server.js
--- src/app/components/routes/landing/landing.component.ts
+++ src/app/components/routes/landing/landing.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Proyecto } from 'src/app/interfaces/proyecto';
 @Component({
   selector: 'app-landing',
   templateUrl: './landing.component.html',
@@ -8,4 +9,14 @@ export class LandingComponent implements OnInit {
   constructor() {}
 
   ngOnInit(): void {}
+
+  proyectos: Proyecto[] = [
+    {
+      titulo: 'Proyecto',
+      descripcion:
+        'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi consequatur molestias error quam fugit reprehenderit, adipisci cumque corporis nihil assumenda inventore repellat. Enim corrupti necessitatibus voluptate quibusdam illum nam assumenda.',
+      imagen: '',
+      opcion: 'Donar',
+    },
+  ];
 }
