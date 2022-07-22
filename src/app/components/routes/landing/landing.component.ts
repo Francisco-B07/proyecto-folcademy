a4caebbba60027714cf62006548f8c2597096c57
Francisco Burgoa <franciscojburgoa@gmail.com>
2022-07-22 17:36:40 -0300

    agregue proyectos

--- src/app/components/routes/landing/landing.component.ts
+++ src/app/components/routes/landing/landing.component.ts
@@ -15,74 +15,74 @@ export class LandingComponent implements OnInit {
   ngOnInit(): void {}
 
   proyectos: Proyecto[] = [
-    // {
-    //   tipo: 'suscripcion',
-    //   imagen: '../../../../assets/bisblick2.jpg',
-    //   titulo: 'Crowfunding por suscripción',
-    //   descripcion:
-    //     'Somos  miembro de Primero Educacion, un grupo de organizaciones de la sociedad jskdifnrlñas, pasomd psamdeun j kd clao lasjbk nd civil que .',
-    //   recaudado: 30000,
-    //   objetivo: 30000,
-    //   opcion: 'DONAR',
-    // },
-    // {
-    //   tipo: 'proyecto',
-    //   imagen: '../../../../assets/bisblick.jpg',
-    //   titulo: 'Ayudemos a Juan',
-    //   descripcion:
-    //     'Lorem ipsum dolor sit amet, consectetur olestias error quam fugit reprehenderit, adipisci cumque corporis nihil assumenda inventore repellat. .',
-    //   recaudado: 130000,
-    //   objetivo: 100000,
-    //   opcion: 'DONAR',
-    // },
-    // {
-    //   tipo: 'proyecto',
-    //   imagen: '../../../../assets/bisblick2.jpg',
-    //   titulo: 'Ayudemos a Dani',
-    //   descripcion:
-    //     'Somos miembro de Primero Educacion, un grupo de organizaciones de la sociedad jskdifnrlñas, pasomd psamdeun j kd clao lasjbk nd civil que trabajamos para .',
-    //   recaudado: 30000,
-    //   objetivo: 100000,
-    //   opcion: 'DONAR',
-    // },
-    // {
-    //   tipo: 'proyecto',
-    //   imagen: '../../../../assets/bisblick3.jpg',
-    //   titulo: 'Proyecto',
-    //   descripcion:
-    //     'Somos miembro de Primero Educacion, un grupo de organizaciones de la sociedad jskdifnrlñas, pasomd psamdeun j kd clao lasjbk nd civil que trabajamos para .',
-    //   recaudado: 5,
-    //   objetivo: 200,
-    //   opcion: 'PARTICIPAR',
-    // },
-    // {
-    //   tipo: 'proyecto',
-    //   imagen: '../../../../assets/bisblick4.jpg',
-    //   titulo: 'Proyecto Escolar',
-    //   descripcion:
-    //     'Somos miembro de Primero Educacion, un grupo de organizaciones de la j kd clao lasjbk nd civil que trabajamos para ubicar a la educación como prioridad nacional.',
-    //   recaudado: 70,
-    //   objetivo: 150,
-    //   opcion: 'PARTICIPAR',
-    // },
-    // {
-    //   tipo: 'proyecto',
-    //   imagen: '../../../../assets/bisblick5.jpg',
-    //   titulo: 'Proyecto',
-    //   descripcion: 'Somos miembro de Primero Educacion',
-    //   recaudado: 40000,
-    //   objetivo: 100000,
-    //   opcion: 'DONAR',
-    // },
-    // {
-    //   tipo: 'proyecto',
-    //   imagen: '../../../../assets/bisblick7.jpg',
-    //   titulo: 'Proyecto',
-    //   descripcion:
-    //     'Somos miembro de Primero Educacion, un grupo de organizaciones de la sociedad jskdifnrlñas, pasomd psamdeun j kd clao lasjbk nd civil que trabajamos para .',
-    //   recaudado: 300,
-    //   objetivo: 300,
-    //   opcion: 'PARTICIPAR',
-    // },
+    {
+      tipo: 'suscripcion',
+      imagen: '../../../../assets/bisblick2.jpg',
+      titulo: 'Crowfunding por suscripción',
+      descripcion:
+        'Somos  miembro de Primero Educacion, un grupo de organizaciones de la sociedad jskdifnrlñas, pasomd psamdeun j kd clao lasjbk nd civil que .',
+      recaudado: 30000,
+      objetivo: 30000,
+      opcion: 'DONAR',
+    },
+    {
+      tipo: 'proyecto',
+      imagen: '../../../../assets/bisblick.jpg',
+      titulo: 'Ayudemos a Juan',
+      descripcion:
+        'Lorem ipsum dolor sit amet, consectetur olestias error quam fugit reprehenderit, adipisci cumque corporis nihil assumenda inventore repellat. .',
+      recaudado: 130000,
+      objetivo: 100000,
+      opcion: 'DONAR',
+    },
+    {
+      tipo: 'proyecto',
+      imagen: '../../../../assets/bisblick2.jpg',
+      titulo: 'Ayudemos a Dani',
+      descripcion:
+        'Somos miembro de Primero Educacion, un grupo de organizaciones de la sociedad jskdifnrlñas, pasomd psamdeun j kd clao lasjbk nd civil que trabajamos para .',
+      recaudado: 30000,
+      objetivo: 100000,
+      opcion: 'DONAR',
+    },
+    {
+      tipo: 'proyecto',
+      imagen: '../../../../assets/bisblick3.jpg',
+      titulo: 'Proyecto',
+      descripcion:
+        'Somos miembro de Primero Educacion, un grupo de organizaciones de la sociedad jskdifnrlñas, pasomd psamdeun j kd clao lasjbk nd civil que trabajamos para .',
+      recaudado: 5,
+      objetivo: 200,
+      opcion: 'PARTICIPAR',
+    },
+    {
+      tipo: 'proyecto',
+      imagen: '../../../../assets/bisblick4.jpg',
+      titulo: 'Proyecto Escolar',
+      descripcion:
+        'Somos miembro de Primero Educacion, un grupo de organizaciones de la j kd clao lasjbk nd civil que trabajamos para ubicar a la educación como prioridad nacional.',
+      recaudado: 70,
+      objetivo: 150,
+      opcion: 'PARTICIPAR',
+    },
+    {
+      tipo: 'proyecto',
+      imagen: '../../../../assets/bisblick5.jpg',
+      titulo: 'Proyecto',
+      descripcion: 'Somos miembro de Primero Educacion',
+      recaudado: 40000,
+      objetivo: 100000,
+      opcion: 'DONAR',
+    },
+    {
+      tipo: 'proyecto',
+      imagen: '../../../../assets/bisblick7.jpg',
+      titulo: 'Proyecto',
+      descripcion:
+        'Somos miembro de Primero Educacion, un grupo de organizaciones de la sociedad jskdifnrlñas, pasomd psamdeun j kd clao lasjbk nd civil que trabajamos para .',
+      recaudado: 300,
+      objetivo: 300,
+      opcion: 'PARTICIPAR',
+    },
   ];
 }
