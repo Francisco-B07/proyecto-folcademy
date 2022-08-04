8210ce0497635ec0b26a8c2134c05c2e9b7bb7e2
Francisco Burgoa <franciscojburgoa@gmail.com>
2022-08-04 14:05:12 -0300

    implemente cambio de contraseña

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -1,19 +1,17 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
-import { MailToFrom, UserI } from '../interfaces/user.interface'
+import { MailToFrom, UserI } from '../interfaces/user.interface';
 import { HttpClient } from '@angular/common/http';
+import { ModificarPassword } from '../interfaces/modificarPassword';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
-
   baseURL = 'https://bisblick-back-muni.herokuapp.com';
- 
 
-  constructor(private http:HttpClient) { 
-  }
+  constructor(private http: HttpClient) {}
 
   // async resetPassword(email:MailToFrom){
   //   try{
@@ -21,11 +19,9 @@ export class AuthService {
   //   }
   //   catch(error){console.log(error)}
   // }
-  resetPassword (email:MailToFrom){
-    return this.http.post(this.baseURL+'/auth'+'/recuperar', email);
+  resetPassword(email: MailToFrom) {
+    return this.http.post(this.baseURL + '/auth' + '/recuperar', email);
   }
-
-
 
   // async login(usuario: UserI) {
   //   try{
@@ -34,10 +30,10 @@ export class AuthService {
   //     return result; }
   //     catch (error) {
   //       console.log(error);
-  //     } 
+  //     }
   // }
-  login (user: UserI) {
-    return this.http.post (this.baseURL+'/auth'+'/login',user)
+  login(user: UserI) {
+    return this.http.post(this.baseURL + '/auth' + '/login', user);
   }
 
   getSession() {
@@ -45,18 +41,20 @@ export class AuthService {
     return currentSession;
   }
   sessionExists(): Boolean {
-    return (this.getSession()) ? true : false;
+    return this.getSession() ? true : false;
   }
 
-  loginByEmail(user: UserI): Observable < any > {
-    
+  loginByEmail(user: UserI): Observable<any> {
     // return this.afAuth.auth.singInWhitEmailAndPassword(email, password);
-    return this.http.post(this.baseURL+'/auth'+'/login',user)
-
+    return this.http.post(this.baseURL + '/auth' + '/login', user);
   }
 
   // logout() {
   //   this.afAuth.auth.singOn();
   // }
-  
+  modificarPassword(usuario: ModificarPassword): Observable<any> {
+    console.log('usuario desde service', usuario);
+
+    return this.http.post(this.baseURL + '/auth/change_password', usuario);
+  }
 }
